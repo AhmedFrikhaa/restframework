1c758ffd301ab464ce8692a05f8e28cae8e0b645
AhmedFrikhaa <ahmed.frikha2@insat.ucar.tn>
2023-05-06 23:40:23 +0200

    first commit

---
+++ snippets/api/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets.api import views
+from snippets.api.views import SnippetViewSet, UserViewSet
+from rest_framework import renderers
+
+"""
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+urlpatterns = [
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/',snippet_detail, name='snippet-detail'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('', views.api_root, name='api-root'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+
+
+]
+"""
+
+# Create a router and register our viewsets with it.
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
